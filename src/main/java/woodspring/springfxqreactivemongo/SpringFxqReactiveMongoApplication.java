8dc4576c2e54eb7af77cacbc5b88bf6b62e20421
woodspring <woodspring.toronto@gmail.com>
2020-11-07 21:13:53 -0500

    first add

---
+++ src/main/java/woodspring/springfxqreactivemongo/SpringFxqReactiveMongoApplication.java
@@ -0,0 +1,13 @@
+package woodspring.springfxqreactivemongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFxqReactiveMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFxqReactiveMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/woodspring/springfxqreactivemongo/SpringFxqReactiveMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package woodspring.springfxqreactivemongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFxqReactiveMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
